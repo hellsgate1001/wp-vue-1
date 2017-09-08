aa46ee96126fbb7f255e615cac325ec7ac5b54a2
Brian Wilson <brianwilson1001@hotmail.com>
2017-09-08 03:26:42 +0100

    Remove redundant service

--- src/components/waypoints-component/waypoints-component.service.js
+++
@@ -1,7 +0,0 @@
-class WaypointsService {
-  get(offset = 0, limit = 30) {
-    return this.$http.get(`http://api.waypoints.briansdojo.co.uk/api/bookmarks/bookmarks/?limit=${limit}&offset=${offset}`);
-  }
-}
-
-export default WaypointsService;
